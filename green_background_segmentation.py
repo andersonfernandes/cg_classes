17d48a818f2db94fccaacb937796be69456db369
Anderson Fernandes <fernandesanderson14@gmail.com>
2019-09-22 20:51:08 -0300

    Add green background segmentation script
    - add img assets
    
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

---
+++ green_background_segmentation.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+image = cv2.imread('./img/green_background.jpg')
+
+lower_green = (0, 140, 0)
+upper_green = (130, 255, 130)
+mask = cv2.inRange(image, lower_green, upper_green)
+
+segmented_image = cv2.bitwise_and(image, image, mask=255-mask)
+
+cv2.imshow("Segmented Image", segmented_image)
+cv2.imshow("Image", image)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
