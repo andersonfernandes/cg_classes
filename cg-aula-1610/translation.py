27b72bdf4a4350ffaad0dcf891e39fda450e9e0f
Anderson Fernandes <fernandesanderson14@gmail.com>
2019-10-16 19:46:42 -0300

    Add translation script
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

---
+++ cg-aula-1610/translation.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+
+image = cv2.imread('./img/brasilia.png')
+height, width = image.shape[:2]
+quarter_height, quarter_width = height/4, width/4
+
+T = np.float32([[1, 0, quarter_width], [0, 1, quarter_height]])
+image_translation = cv2.warpAffine(image, T, (width, height))
+
+cv2.imshow('Translation', image_translation)
+
+cv2.waitKey()
+cv2.destroyAllWindows()
+
